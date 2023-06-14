2a95243c4f32a6c935d4a8e4df003216d0cca72e
mateoU1297 <castro.mateo@correounivalle.edu.co>
2023-06-14 17:14:53 -0500

    se agrega al proyecto jwt

--- src/main/java/com/udemy/springboot/di/app/SpringSecurityConfig.java
+++ src/main/java/com/udemy/springboot/di/app/SpringSecurityConfig.java
@@ -3,21 +3,22 @@ package com.udemy.springboot.di.app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.udemy.springboot.di.app.auth.handler.LoginSuccessHandler;
+import com.udemy.springboot.di.app.auth.filter.JWTAuthenticationFilter;
 import com.udemy.springboot.di.app.models.service.JpaUserDetailsService;
 
-@Configuration
 @EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@Configuration
 public class SpringSecurityConfig {
-
-	@Autowired
-	private LoginSuccessHandler successHandler;
 
 	@Bean
 	public static BCryptPasswordEncoder passwordEncoder() {
@@ -28,24 +29,24 @@ public class SpringSecurityConfig {
 	private JpaUserDetailsService userDetailService;
 
 	@Autowired
+	private AuthenticationConfiguration authenticationConfiguration;
+
+	@Bean
+	public AuthenticationManager authenticationManager() throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+	@Autowired
 	public void userDetailsService(AuthenticationManagerBuilder build) throws Exception {
 		build.userDetailsService(userDetailService).passwordEncoder(passwordEncoder());
 	}
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests((authz) -> {
-			try {
-				authz.requestMatchers("/", "/css/**", "/js/**", "/images/**", "/listar**", "/locale", "/api/clientes/**").permitAll().anyRequest()
-						.authenticated().and().formLogin().successHandler(successHandler).loginPage("/login")
-						.permitAll().and().logout().permitAll().and().exceptionHandling()
-						.accessDeniedPage("/error_403");
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		});
-
+		http.authorizeHttpRequests().requestMatchers("/", "/css/**", "/js/**", "/images/**", "/listar**", "/locale")
+				.permitAll().anyRequest().authenticated().and()
+				.addFilter(new JWTAuthenticationFilter(authenticationConfiguration.getAuthenticationManager())).csrf()
+				.disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		return http.build();
-
 	}
 }
---
+++ src/main/java/com/udemy/springboot/di/app/auth/filter/JWTAuthenticationFilter.java
@@ -0,0 +1,78 @@
+package com.udemy.springboot.di.app.auth.filter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+	private AuthenticationManager authenticationManager;
+	
+	public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
+		setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher("/api/login", "POST"));
+	}
+	
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException {
+		
+		String username = obtainUsername(request);
+		String password = obtainPassword(request);
+		
+		if(username == null) {
+			username = "";
+		}
+		
+		if(password == null) {
+			password = "";
+		}
+		
+		username = username.trim();
+		
+		UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
+		
+		return authenticationManager.authenticate(authToken);
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+		
+		String username = authResult.getName();
+		
+		String token = Jwts.builder()
+				.setSubject(username)
+				.signWith(SignatureAlgorithm.HS512, "Alguna.Clave.Secreta.123456".getBytes())
+				.compact();
+		
+		response.addHeader("Authorization", "Bearer " + token);
+		
+		Map<String, Object> body = new HashMap<String, Object>();
+		body.put("token", token);
+		body.put("user", (User) authResult.getPrincipal());
+		body.put("mensaje", String.format("Hola %s, has iniciado sesión con éxito!", username));
+		
+		response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+		response.setStatus(200);
+		response.setContentType("application/json");
+	}
+
+}
